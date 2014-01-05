af64c6f14208d2bf56ceccc647566043c5af40d9
Brian J. Creasy <genetik@gmail.com>
2014-01-04 19:52:38 -0600

    more couchdb messing around

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var nano = require('nano')('http://localhost:5984');
 var gaslog = nano.db.use('gaslog');
 
-nano.db.get('gaslog', function(err, body) {
-	if (!err) {
-		console.log(body);
-	}
+gaslog.view('basic', 'basic', function(err, body) {
+  if (!err) {
+    body.rows.forEach(function(doc) {
+      console.log(doc.key);
+      console.log(doc.value);
+    });
+  }
 });
