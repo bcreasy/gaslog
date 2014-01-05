adeb1b21a9c7e88de99289d2b700d9425665c1e7
Brian J. Creasy <genetik@gmail.com>
2014-01-04 21:03:57 -0600

    format gas price with a custom filter
    switched to unminimized angular.js to help with debugging

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,17 @@
 'use strict';
 
-var gaslog = angular.module('gaslog', []);
+var gaslog = angular.module('gaslog', ['filters']);
 
 gaslog.controller('LogEntries', function($scope, $http) {
   $http.get('gaslog.json').success(function(data) {
 		$scope.fillups = data;
   });
   $scope.orderProp = 'date';
+});
+
+angular.module('filters', []).
+filter('gasprice', function ($filter) {
+  return function (input) {
+    return '$' + $filter('number')(input, 3);
+  };
 });
